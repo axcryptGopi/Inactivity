6058ca0d6f2c22515a93524de88a0607852045c0
Gopinath <Gopinath.b@axcrypt.net>
2025-11-04 16:41:59 +0530

    sample

---
+++ MauiApp1/Platforms/Windows/ScreenLockDetector.cs
@@ -0,0 +1,30 @@
+﻿using MauiApp1.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Platforms.Windows.Services
+{
+    public class ScreenLockDetector : IScreenLockDetector
+    {
+        public event EventHandler ScreenLocked;
+        public event EventHandler ScreenUnlocked;
+
+        public ScreenLockDetector()
+        {
+            ScreenDetector.Detector = this;
+        }
+
+        private void LockDetector_ScreenUnlocked(object? sender, EventArgs e)
+        {
+            ScreenLocked?.Invoke(this, e);
+        }
+
+        private void LockDetector_ScreenLocked(object? sender, EventArgs e)
+        {
+            ScreenUnlocked?.Invoke(this, e);
+        }
+    }
+}
